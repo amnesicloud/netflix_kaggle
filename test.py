dc4abb5849f723db1d47044149985aefc463b2f5
Yunzhou Zhang <yunzhou619@gmail.com>
2018-01-10 17:42:33 +0800

    preprocess

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os, csv
 import numpy as np
 from mpi4py import MPI
 
+file_num = 1
 use_station = 'wukong'
 if use_station == 'local':
     dataset_dir = '/home/yuzhuoran/Documents/datasets'
@@ -17,33 +18,34 @@ if use_station == 'master':
 
 data_dir_path = os.path.join(dataset_dir, 'netflix-prize-data')
 
-data_path1 = os.path.join(data_dir_path, 'combined_data_1.txt')
-data_path2 = os.path.join(data_dir_path, 'combined_data_2.txt')
-data_path3 = os.path.join(data_dir_path, 'combined_data_3.txt')
-data_path4 = os.path.join(data_dir_path, 'combined_data_4.txt')
-
-data_paths = [data_path1, data_path2, data_path3, data_path4]
+# data_path1 = os.path.join(data_dir_path, 'combined_data_1.txt')
+# data_path2 = os.path.join(data_dir_path, 'combined_data_2.txt')
+# data_path3 = os.path.join(data_dir_path, 'combined_data_3.txt')
+# data_path4 = os.path.join(data_dir_path, 'combined_data_4.txt')
+#
+# data_paths = [data_path1, data_path2, data_path3, data_path4]
 file_lines = [24058263, 26982302,22605786, 26851926]
 
 
 # data_path = os.path.join(data_dir_path, 'combined_data_sample.txt')
-user_rating_mean_output_path = os.path.join(data_dir_path, 'user_rating_means.csv')
+data_path = os.path.join(data_dir_path, 'combined_data_%d.txt'%file_num)
+user_rating_mean_output_path = os.path.join(data_dir_path, 'user_rating_means%d.csv'%file_num)
 user_rating_mean_dict = {}
 user_ratings = []
-for ind, data_path in enumerate(data_paths):
-    line_count = 0
-    with open(data_path, 'r') as r:
-        for line in r:
-            line_count += 1
-            if ':' in line:
-                if not len(user_ratings) == 0:
-                    rating_mean = np.mean(user_ratings)
-                    user_rating_mean_dict[user_id] = rating_mean
-                user_id = int(line.split(':')[0])
-            else:
-                user_ratings.append(int(line.split(',')[1]))
-            if line_count % 20000 == 0:
-                print('reading file %d, line %d / %d' %(ind, line_count, file_lines[ind]))
+
+line_count = 0
+with open(data_path, 'r') as r:
+    for line in r:
+        line_count += 1
+        if ':' in line:
+            if not len(user_ratings) == 0:
+                rating_mean = np.mean(user_ratings)
+                user_rating_mean_dict[user_id] = rating_mean
+            user_id = int(line.split(':')[0])
+        else:
+            user_ratings.append(int(line.split(',')[1]))
+        if line_count % 20000 == 0:
+            print('reading file %d, line %d / %d' %(line_count, file_lines[file_num-1]))
 
 
 with open(user_rating_mean_output_path, 'w') as w:
