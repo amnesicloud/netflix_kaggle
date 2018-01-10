288801fe386cdf80c3cd3120b212b2f68f20126c
Yunzhou Zhang <yunzhou619@gmail.com>
2018-01-10 10:12:18 +0800

    preprocess

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-import os
+import os, csv
+import numpy as np
+from mpi4py import MPI
 
 use_station = 'local'
 if use_station == 'local':
@@ -15,3 +17,40 @@ if use_station == 'master':
 
 data_dir_path = os.path.join(dataset_dir, 'netflix-prize-data')
 
+data_path1 = os.path.join(data_dir_path, 'combined_data_1.txt')
+data_path2 = os.path.join(data_dir_path, 'combined_data_2.txt')
+data_path3 = os.path.join(data_dir_path, 'combined_data_3.txt')
+data_path4 = os.path.join(data_dir_path, 'combined_data_4.txt')
+
+data_paths = [data_path1, data_path2, data_path3, data_path4]
+file_lines = [24058263, 26982302,22605786, 26851926]
+
+
+# data_path = os.path.join(data_dir_path, 'combined_data_sample.txt')
+user_rating_mean_output_path = os.path.join(data_dir_path, 'user_rating_means.csv')
+user_rating_mean_dict = {}
+user_ratings = []
+for ind, data_path in enumerate(data_paths):
+    line_count = 0
+    with open(data_path, 'r') as r:
+        for line in r:
+            line_count += 1
+            if ':' in line:
+                if not len(user_ratings) == 0:
+                    rating_mean = np.mean(user_ratings)
+                    user_rating_mean_dict[user_id] = rating_mean
+                user_id = int(line.split(':')[0])
+            else:
+                user_ratings.append(int(line.split(',')[1]))
+            if line_count % 20000 == 0:
+                print('reading file %d, line %d / %d' %(ind, line_count, file_lines[ind]))
+
+
+with open(user_rating_mean_output_path, 'w') as w:
+    csvw = csv.writer(w)
+    user_count = 0
+    for user_id in user_rating_mean_dict:
+        user_count += 1
+        csvw.writerow([user_id, user_rating_mean_dict[user_id]])
+        if user_count % 1000 == 0:
+            print('finished writing user %d / %d' % (user_count, len(user_rating_mean_dict)))
